3d5db43951e7ac4d3f54d5ea8646574661e0d1ce
Andrew <ael2193@columbia.edu>
2022-02-05 15:04:54 -0500

    update predict_page

--- predict_page.py
+++ predict_page.py
@@ -3,6 +3,20 @@ import pickle
 import numpy as np
 import pandas as pd
 
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import StratifiedKFold 
+from sklearn.model_selection import GridSearchCV
+from sklearn import metrics
+import seaborn as sns
+import matplotlib.pyplot as plt
+pd.set_option("max_columns", 100)
+from imblearn import over_sampling
+from imblearn.over_sampling import SMOTE
+from collections import Counter
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_selection import SelectFromModel
+
 
 def load_model():
     with open('saved_steps.pkl', 'rb') as file:
